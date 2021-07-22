03391f2e4ec10f699a41b82cd8ef8de131bad1c5
Beto Drumond <humbertowdrumond@gmail.com>
2021-07-21 22:14:41 -0300

    Add binding

--- app/src/main/java/com/betow/mymoviesapp/MainActivity.kt
+++ app/src/main/java/com/betow/mymoviesapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.betow.mymoviesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.betow.mymoviesapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
 
         println(ehUmBomFilme("Vingadores:Endgame"))
         criarNumeros()
